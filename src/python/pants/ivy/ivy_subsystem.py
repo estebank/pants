edcfcf1c2206aeab9e1398bd1b5a6252b9a00cbc
Chris Heisterkamp <cheister@squareup.com>
2018-05-25 12:37:21 -0700

    Separate the resolution cache and repository cache in Ivy

--- src/python/pants/ivy/ivy_subsystem.py
+++ src/python/pants/ivy/ivy_subsystem.py
@@ -40,6 +40,10 @@ class IvySubsystem(Subsystem):
              help='The version of ivy to fetch.')
     register('--cache-dir', advanced=True, default=os.path.expanduser('~/.ivy2/pants'),
              help='Directory to store artifacts retrieved by Ivy.')
+    register('--resolution-cache-dir', advanced=True,
+             help='Directory to store Ivy resolution artifacts.')
+    register('--repository-cache-dir', advanced=True,
+             help='Directory to store Ivy repository artifacts.')
     register('--ivy-settings', advanced=True,
              help='Location of XML configuration file for Ivy settings.')
     register('--bootstrap-ivy-settings', advanced=True,
@@ -93,3 +97,15 @@ class IvySubsystem(Subsystem):
   def _parse_proxy_string(self, proxy_string):
     parse_result = urllib.parse.urlparse(proxy_string)
     return parse_result.hostname, parse_result.port
+
+  def resolution_cache_dir(self):
+    if self.get_options().resolution_cache_dir:
+      return self.get_options().resolution_cache_dir
+    else:
+      return self.get_options().cache_dir
+
+  def repository_cache_dir(self):
+    if self.get_options().repository_cache_dir:
+      return self.get_options().repository_cache_dir
+    else:
+      return self.get_options().cache_dir
